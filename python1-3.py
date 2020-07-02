0cf9a6f6b2ada2fd5b5d57e56f77e8bf1e442887
f-hikaru0604 <h-fukadu@b-it.co.jp>
2020-07-02 15:05:24 +0900

    refs #1:ソースコード修正

--- python1-3.py
+++ python1-3.py
@@ -2,13 +2,13 @@ print("身長を入力してください")
 height = int(input())/100
 print("体重を入力してください")
 weight = int(input())
-BMI = weight / (height**2)
-if BMI < 18.5 :
-    taikei = "やせ"
-elif BMI < 25 :
-    taikei = "標準"
-elif BMI < 30 :
-    taikei = "肥満"
+bmi = weight / (height ** 2)
+if bmi < 18.5 :
+    figure = "やせ"
+elif bmi < 25 :
+    figure = "標準"
+elif bmi < 30 :
+    figure = "肥満"
 else:
-    taikei = "高度肥満"
+    figure = "高度肥満"
 print("あなたは「" + str(taikei) + "」です。")
