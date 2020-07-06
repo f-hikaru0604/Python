bec4c8e7c7e9a4f080e137300d9b0dfa415dca91
f-hikaru0604 <h-fukadu@b-it.co.jp>
2020-07-06 11:30:41 +0900

    refs #1:update2

--- python1_test4.py
+++ python1_test4.py
@@ -1,13 +1,15 @@
 borrowing = 250000
 annual_rate = 0.14
-repayment_amount = 30000
+repayment = 30000
 month = 1
 while 0 < borrowing:
-    month_interest = borrowing * annual_rate / 12
-    borrowing += month_interest
-    if borrowing < repayment_amount:
-        print(str(month),"ヶ月目：返済額=",str(borrowing),"、返済完了")
-        break 
-    borrowing = borrowing - repayment_amount
-    print(str(month),"ヶ月目：返済額=",str(repayment_amount),"円、残り",str(borrowing),"円")
+    borrowing = borrowing * (1 + annual_rate / 12)
+    if repayment < borrowing:
+        borrowing -= repayment
+        print(str(month),"ヶ月目：返済額=",str(repayment),"円、残り",str(borrowing),"円")
+        
+    else: 
+        repayment = borrowing
+        borrowing = 0
+        print(str(month),"ヶ月目：返済額=",str(repayment),"、返済完了")
     month += 1
